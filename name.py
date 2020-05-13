f8bf7c54f1a6f240420fb9eed3a80f0f34820a75
ImLegend4658 <MrAziz2005@gmail.com>
2020-05-14 00:11:52 +0300

    Add files via upload

---
+++ name.py
@@ -0,0 +1,15 @@
+def names():
+    name = input("Enter your name: ")
+    return name
+
+def age():
+    age = input("Enter your Age: ")
+    return age
+
+def dis():
+    u = names()
+    a = age()
+    print(f"your name is: {u} and your age is: {a}")
+# u = names()
+# a = age()
+display = dis()
